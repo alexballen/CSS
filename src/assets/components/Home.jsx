99d69d5c5d57b42ceb28090c14e960b662877e8f
Alex <alexballen53@gmail.com>
2023-04-27 13:53:08 -0500

    Push

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -1,4 +1,5 @@
 import s from "./Home.module.css";
+import img from "../react.svg";
 
 const Home = () => {
   return (
@@ -10,22 +11,31 @@ const Home = () => {
         <h2 className={s.footer_title}>Hola mundo 2</h2>
       </footer> */}
       {/* <div className={s.elem}></div> */}
-      <div className={s.block}>Soy un div</div>
-      <div className={s.block}>Soy un div</div>
-
-      <a href="#" className={s.inline}>
-        Soun un elemento a
-      </a>
-      <a href="#" className={s.inline}>
-        Soun un elemento a
-      </a>
-      <a href="#" className={s.inline}>
-        Soun un elemento a
-      </a>
-
-      <a href="#" className={s.button}>
-        Soy un boton
-      </a>
+      {/* <div className={s.black}></div> */}
+      {/* <div className={s.pacman}></div> */}
+      {/* <div className={s.card}>
+        <img src={img} alt="imag" className={s.img} />
+        <h2 className={s.title}>Hola mundo bienvenido a Cali</h2>
+        <p className={s.paragraph}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Delectus, fugiat
+          reprehenderit, accusantium odit facere, veniam corporis exercitationem
+          aperiam magni itaque saepe cupiditate at? Eveniet optio ratione quasi
+          quod, iste culpa!
+        </p>
+        <p className={s.paragrafphdos}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum omnis
+          veniam impedit facere consequuntur perspiciatis cupiditate iure
+          deserunt nisi? Laborum doloribus aspernatur necessitatibus vel
+          veritatis eveniet alias beatae ad aperiam?
+        </p>
+      </div> */}
+      <div className={s.container}>
+        <h2 className={s.titleuno}>Soy el primer elemento</h2>
+        <h2 className={s.titledos}>Soy el segundo elemento</h2>
+        <h2 className={s.titletres}>Soy el tercer elemento</h2>
+        <h2 className={s.titlecuatro}>Soy el cuarto elemento</h2>
+      </div>
     </>
   );
 };
